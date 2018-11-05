f41ddd80f0638cec47f887b0c0be261d8f85ca61
Deeper-Lee <984130957@qq.com>
2018-11-05 16:14:09 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ArrRoutingModule } from './/arr-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ArrRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
